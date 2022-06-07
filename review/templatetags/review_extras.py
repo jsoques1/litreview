782112c31d27dfdb69394055e0e65226824fbca1
jsoques1 <joseph.soques@gmail.com>
2022-06-07 13:19:06 +0200

    draft implementation for stream and posts

--- review/templatetags/review_extras.py
+++ review/templatetags/review_extras.py
@@ -1,9 +1,4 @@
 from django import template
-from django.utils import timezone
-
-MINUTE = 60
-HOUR = 60 * MINUTE
-DAY = 24 * HOUR
 
 register = template.Library()
 
@@ -12,24 +7,3 @@ register = template.Library()
 def model_type(value):
     return type(value).__name__
 
-
-@register.simple_tag(takes_context=True)
-def get_reviewing_user(context, user):
-    if user == context['user']:
-        return 'vous'
-    return user.username
-
-
-@register.filter
-def get_reviewed_at(posted_at):
-    seconds_ago = (timezone.now() - posted_at).total_seconds()
-    if seconds_ago <= HOUR:
-        return f'Publié il y a {int(seconds_ago // MINUTE)} minutes.'
-    elif seconds_ago <= DAY:
-        return f'Publié il y a {int(seconds_ago // HOUR)} heures.'
-    return f'Publié le {posted_at.strftime("%d %b %y à %Hh%M")}'
-
-
-@register.filter()
-def range_star(default=5):
-    return range(1, default + 1, 1)
